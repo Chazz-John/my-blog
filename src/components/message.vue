cf644110e013bb43ef2ada77f214a99429fa59ce
chazz_john <chazz0504@qq.com>
2021-08-11 17:57:33 +0800

    feature(http封装文件,message组件,状态存储):
    (1)添加请求响应拦截器
    (2)添加全局状态管理
    (3)添加全局消息提醒组件

---
+++ src/components/message.vue
@@ -0,0 +1,34 @@
+<!--消息提醒全局消息提醒组件-->
+<template>
+  <div class="message">
+    <template v-for="(item, index) in $store.state.msgPool">
+      <div class="d-flex justify-center" transition="slide-y-transition" :key="index">
+        <template v-if="item.light === false" transition="scale-transition">
+          <v-alert dense :type="item.type">{{ item.message }}</v-alert>
+        </template>
+        <template v-if="item.light === true" transition="scale-transition">
+          <v-alert dense :type="item.type" outlined>{{item.message}}</v-alert>
+        </template>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "message.vue"
+}
+</script>
+
+<style scoped>
+.message {
+  left: 50%;
+  z-index: 9999;
+  display: flex;
+  position: fixed;
+  min-width: 300px;
+  max-width: 460px;
+  flex-direction: column;
+  transform: translateX(-50%);
+}
+</style>
